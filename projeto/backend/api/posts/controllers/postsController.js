feea99f001a2cdadc1b52466ab7f47953f0cb22e
gadiel <gadiel.santos@grupomalta.com.br>
2018-12-04 13:45:13 -0200

    separação da responsabilidade de posts

--- projeto/backend/api/posts/controllers/postsController.js
+++ projeto/backend/api/posts/controllers/postsController.js
@@ -1,100 +1,37 @@
-const pagination = require('../../../tools/pagination')
-const axios = require('axios')
-let Model;
+const axios = require('axios');
+const Crud = require('../../../tools/crud')
 
-class ModelController {
+class PostController extends Crud{
   constructor(model) {
-    if (model) {
-      Model = model
-    }
-    else console.log('no model provided')
+    super()
+    this.Model = model
   }
 
-  getAll (req, res) {
-    pagination(Model, {}, req.query)
-      .then(results => res.send({ results }))
-      .catch(err => {
-        console.log(err)
+  info(req, res) {
+    this.Model.findOne({
+        _id: req.params.id
+      })
+      .then(results => {
+        axios.get(`http://www.omdbapi.com/?t=${results.movie_title}&apikey=3d2b356f&plot=full`)
+          .then(response => {
+            this.Model.imdbInfo = response.data
+            res.status(200).send(Model)
+          })
+      }).catch(err => {
         res.status(500).send(`Internal server error - ${req.path}`);
-    })
+      })
   }
-  
-  createOne (req, res) {
-    if (!req.body) {
-      return res.status(400).send('wrong request');
-    }
 
-    try {
-      Model.create(req.body).then(() => {
-
-      })
-      res.redirect('/Model')
-    } catch (e) {
-      res.render('Model/nova', {
-        errors: Object.keys(e.errors)
-      })
-    }
-  }
-  
-  novaForm (req, res) {
-    res.render('Model/nova', {
-      errors: []
-    })
-  }
-  
-  excluir (req, res) {
-    await Model.remove({
-      _id: req.params.id
-    })
-    res.redirect('back')
-  }
-  
-  editarProcess (req, res) {
-    try {
-      await Model.updateOne({ _id: req.params.id }, { $set: req.body })
-      res.redirect(`/info/${req.params.id}`)
-    } catch (e) {
-      console.log(e)
-      res.render('Model/editar', {
-        Model,
-        errors: Object.keys(e.errors)
-      })
-    }
-  }
-  
-  editarForm (req, res) {
-    const Model = await Model.findOne({
-      _id: req.params.id
-    })
-    res.render('Model/editar', {
-      Model,
-      
-      errors: []
-    })
-  }
-  
-  info (req, res) {
-    try {
-      const Model = await Model.findOne({ _id: req.params.id })
-      const response = await axios.get(`http://www.omdbapi.com/?t=${Model.movie_title}&apikey=3d2b356f&plot=full`)
-      Model.imdbInfo = response.data
-      res.render('Model/info', {
-        Model
-      })
-    } catch (error) {
-      res.render('Model/error')
-    }
-  }
-  
   addComentario (req, res) {
-    try {
-      await Model.updateOne({ _id: req.params.id }, { $push: { comments: req.body } })
-      res.redirect(`/info/${req.params.id}`)
-    } catch(e) {
-      res.render('Model/error')
-    }
+    this.Model.updateOne({ _id: req.params.id }, { $push: { comments: req.body } })
+        .then(() => {
+          res.status(200).send('success')
+        })
+    .catch(e => {
+      res.status(500).send(`Internal server error - ${req.path}`);
+    })
   }
   
 }
 
-module.exports = ModelController
+module.exports = PostController;
---
+++ projeto/backend/tools/crud.js
@@ -0,0 +1,47 @@
+const pagination = require('./tools/pagination')
+class Crud {
+  constructor() {
+    this.Model = null;
+  }
+
+  getAll (req, res) {
+    pagination(this.Model, {}, req.query)
+      .then(results => res.send({ results }))
+      .catch(err => {
+        console.log(err)
+        res.status(500).send(`Internal server error - ${req.path}`);
+    })
+  }
+  
+  createOne (req, res) {
+    if (!req.body) {
+      return res.status(400).send('wrong request');
+    }
+    this.Model.create(req.body)
+      .then(() => res.status(200).send('success'))
+      .catch(e => {
+        console.log(e)
+        res.status(500).send(`Internal server error - ${req.path}`);
+      })
+  }
+  
+  deleteOne (req, res) {
+    this.Model.remove({ _id: req.params.id })
+      .then(() => res.status(200).send('success delete'))
+      .catch(e => {
+        console.log(e)
+        res.status(500).send(`Internal server error - ${req.path}`)
+      })
+  }
+  
+  editOne (req, res) {
+   this.Model.updateOne({ _id: req.params.id }, { $set: req.body })
+    .then(() => res.status(200).send('success'))
+    .catch (e => {
+      console.log(e)
+      res.status(500).send(`Internal server error - ${req.path}`)
+    })
+  }
+}
+
+module.exports = Crud
