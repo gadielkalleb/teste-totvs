35bb76a9093f3179db4260d34da356a9cd8c0f46
gadiel <gadiel.santos@grupomalta.com.br>
2018-12-05 09:54:21 -0200

    refactoring

--- backend/tools/insertData.js
+++ backend/tools/insertData.js
@@ -3,34 +3,27 @@ const { MongoClient } = require('mongodb')
 const { db } = require('../config')
 const data = require('../data_json/MOCK_DATA.json')
 
-const mongodb = (db) => new Promise((resolve,reject) => {
-  MongoClient.connect(db.url, db.options,(err,mdb) => {
-      if(err) return reject(err);
-      resolve(mdb);
-  });
-});
-
-module.exports = () => {
+module.exports = async () => {
   console.time('tempo de excução script insertData ')
-  mongodb(db)
-    .then(async mongo => {
-      let posts = mongo.db().collection('posts');
-      if (await posts.find({}).count() <= 0) {
-        console.log('db vazia restaurando backup')
-        _async.eachSeries(data, (reg, callback) => {
-          posts.insertOne(reg, err => {
-            if (err) {
-              console.log('err loading data from json', err)
-            } else {
-              callback();
-            }
-          })
+  try {
+    const  mongo = await MongoClient.connect(db.url, db.options)
+    const posts = mongo.db().collection('posts')
+    if (await posts.find({}).count() <= 0) {
+      _async.eachSeries(data, (reg, callback) => {
+        posts.insertOne(reg, err => {
+          if (err) {
+            console.log('err loading data from json', err)
+          } else {
+            callback();
+          }
         })
-        console.log('load dos backup ok')
-      } else {
-        console.log('db já esta polulado não é necessario carregar backup')
-      }
-    })
-    .catch(e => console.log('erro ao processar o loding de dados ', e))
-    console.timeEnd('tempo de excução script insertData ')
+      })
+      console.log('load dos backup ok')
+    } else {
+      console.log('db já esta polulado não é necessario carregar backup')
+    }
+  } catch (e) {
+    console.log('erro ao processar o loding de dados ', e);
+  }
+  console.timeEnd('tempo de excução script insertData ')
 }
