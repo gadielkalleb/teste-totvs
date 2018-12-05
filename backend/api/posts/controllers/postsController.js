18f1e34b688d143a877580baba659f5165aed758
gadiel <gadiel.santos@grupomalta.com.br>
2018-12-05 10:16:51 -0200

    melhorias no codigo

--- backend/api/posts/controllers/postsController.js
+++ backend/api/posts/controllers/postsController.js
@@ -1,35 +1,36 @@
 const axios = require('axios');
-const Crud = require('../../../tools/crud')
+const Crud = require('../../../tools/crud');
 
-class PostController extends Crud{
-  constructor (model) {
-    super()
-    this.Model = model
+class PostController extends Crud {
+  constructor(model) {
+    super();
+    this.Model = model;
   }
 
   info(req, res) {
-    this.Model.findOne({
-        _id: req.params.id
+    this.Model
+      .findOne({ _id: req.params.id })
+      .then(async (r) => {
+        const results = r;
+        const response = await axios.get(`http://www.omdbapi.com/?t=${results.movie_title}&apikey=3d2b356f&plot=full`);
+        results.imdbInfo = response.data;
+        res.status(200).send(results);
       })
-      .then(results => {
-        axios.get(`http://www.omdbapi.com/?t=${results.movie_title}&apikey=3d2b356f&plot=full`)
-          .then(response => {
-            this.Model.imdbInfo = response.data
-            res.status(200).send(Model)
-          })
-      }).catch(err => {
+      .catch((err) => {
+        console.log(err);
         res.status(500).send(`Internal server error - ${req.path}`);
-      })
+      });
   }
 
-  addComentario (req, res) {
+  addComentario(req, res) {
     this.Model.updateOne({ _id: req.params.id }, { $push: { comments: req.body } })
-        .then(() => {
-          res.status(200).send('success')
-        })
-    .catch(e => {
-      res.status(500).send(`Internal server error - ${req.path}`);
-    })
+      .then(() => {
+        res.status(200).send('success');
+      })
+      .catch((e) => {
+        console.log(e);
+        res.status(500).send(`Internal server error - ${req.path}`);
+      });
   }
 }
 
