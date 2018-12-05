8f08fbea58a05b283bd2ecd07fd89740e4c610ef
gadiel <gadiel.santos@grupomalta.com.br>
2018-12-05 19:02:24 -0200

    alteração da rota de posts

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const { posts, authors, sobre } = require('./routes');
 
-router.use('/', posts);
+router.use('/posts', posts);
 router.use('/author', authors);
 router.use('/sobre', sobre);
 
