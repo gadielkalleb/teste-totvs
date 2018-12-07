5d855f29474cc24323fc515213539f384ba59311
gadiel <gadiel.santos@grupomalta.com.br>
2018-12-07 11:53:51 -0200

    passando o radix para o parse int

--- backend/tools/pagination.js
+++ backend/tools/pagination.js
@@ -1,11 +1,11 @@
 module.exports = async (model, conditions, params) => {
   const total = await model.countDocuments(conditions);
-  const pageSize = parseInt(params.pageSize) || 10;
-  const currentPage = parseInt(params.page) || 0;
+  const pageSize = parseInt(params.pageSize, 10) || 10;
+  const currentPage = parseInt(params.page, 10) || 0;
   const pagination = {
     currentPage,
     pageSize,
-    pages: parseInt(total / pageSize),
+    pages: parseInt(total / pageSize, 10),
   };
 
   const results = await model
