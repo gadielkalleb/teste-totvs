d38a452c15ef7f9e49c45d05609abe4c4effcca4
gadielkalleb <gadiel_kalleb@hotmail.com>
2018-12-08 00:05:03 -0200

    remoção de algumas rotas de authors

--- backend/api/routes/authors.js
+++ backend/api/routes/authors.js
@@ -5,8 +5,5 @@ const authorsController = require('../authors');
 const router = express.Router();
 
 router.post('/auth', authorsController.authAuthors);
-router.get('/:id', authorsController.getOne);
-router.put('/:id', authorsController.editOne);
-router.delete('/:id', authorsController.deleteOne);
 
 module.exports = router;
